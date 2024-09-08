1e4104e17dd723e0139b20b3ba6099adf8ff6bf7
Sreji01 <marko.srejic01@gmail.com>
2024-09-08 23:52:54 +0200

    Napravljen projekat

---
+++ my-podcast-platform-react/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
